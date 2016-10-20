e945447a503be9f1c3cf0399e1ed83c7dd4a7fe2
shafiq <shafiqabs@gmail.com>
2016-10-20 19:16:57 +0600

    URL Issue resolved

--- src/Frontend/FrontentBundle/Controller/EcommerceController.php
+++ src/Frontend/FrontentBundle/Controller/EcommerceController.php
@@ -1,31 +1,12 @@
 <?php
 
 namespace Frontend\FrontentBundle\Controller;
-use Product\Bundle\ProductBundle\Entity\Category;
-use Setting\Bundle\ToolBundle\Entity\Branding;
-use Product\Bundle\ProductBundle\Entity\Product;
-use Setting\Bundle\ToolBundle\Entity\SubscribeEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Syndicate\Bundle\ComponentBundle\Entity\Education;
-use Syndicate\Bundle\ComponentBundle\Entity\Vendor;
+
 
 class EcommerceController extends Controller
 {
 
-
-    public function paginate($entities,$limit = 10)
-    {
-
-        $paginator  = $this->get('knp_paginator');
-        $pagination = $paginator->paginate(
-            $entities,
-            $this->get('request')->query->get('page', 1)/*page number*/,
-            $limit /*limit per page*/
-        );
-        return $pagination;
-    }
 
     public function indexAction()
     {
@@ -41,31 +22,6 @@ class EcommerceController extends Controller
             'sliders'       => $sliders,
 
         ));
-    }
-
-
-
-    public function searchAction()
-    {
-
-        return $this->render('FrontendBundle:Default:search.html.twig');
-    }
-
-    public function getCaptcha()
-    {
-       // session_start();
-        $text = rand(10000,99999);
-        $_SESSION["vercode"] = $text;
-        $height = 25;
-        $width = 65;
-
-        $image_p = imagecreate($width, $height);
-        $black = imagecolorallocate($image_p, 0, 107, 143);
-        $white = imagecolorallocate($image_p, 255, 255, 255);
-        $font_size = 14;
-
-        imagestring($image_p, $font_size, 5, 5, $text, $white);
-        imagejpeg($image_p, null, 80);
     }
 
 }
