fcced94adaa88f6e5c7c5d5b680e8f7939392cc7
shafiq <shafiqabs@gmail.com>
2017-07-31 17:47:05 +0600

    E-commerce add product modify.

--- src/Frontend/FrontentBundle/Resources/public/template/js/e-commerce.js
+++ src/Frontend/FrontentBundle/Resources/public/template/js/e-commerce.js
@@ -1,5 +1,5 @@
-var owlCategory = $("#category-slider-1");
-owlCategory.owlCarousel({
+var owlCategoryx = $(".category-slider");
+owlCategoryx.owlCarousel({
     items: 4 ,
     slideSpeed : 5000,
     itemsCustom : false,
@@ -19,127 +19,12 @@ owlCategory.owlCarousel({
     pagination : false,
 });
 
-// Custom Navigation Events
-$("#nextCatItem-1").click(function(){
-    owlCategory.trigger('owl.next');
-});
-$("#prevCatItem-1").click(function(){
-    owlCategory.trigger('owl.prev');
-});
-
-var owlCategory2 = $("#category-slider-2");
-owlCategory2.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevCatItem-2' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextCatItem-2' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-// Custom Navigation Events
-$("#nextCatItem-2").click(function(){
-    owlCategory2.trigger('owl.next');
-});
-$("#prevCatItem-2").click(function(){
-    owlCategory2.trigger('owl.prev');
-});
-
-var owlCategory3 = $("#category-slider-3");
-owlCategory.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevCatItem-3' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextCatItem-3' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-// Custom Navigation Events
-$("#nextCatItem-3").click(function(){
-    owlCategory3.trigger('owl.next');
-});
-$("#prevCatItem-3").click(function(){
-    owlCategory3.trigger('owl.prev');
-});
-
-var owlCategory4 = $("#category-slider-4");
-owlCategory4.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevCatItem-4' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextCatItem-4' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-// Custom Navigation Events
-$("#nextCatItem-4").click(function(){
-    owlCategory4.trigger('owl.next');
-});
-$("#prevCatItem-4").click(function(){
-    owlCategory4.trigger('owl.prev');
-});
-
-var owlCategory5 = $("#category-slider-5");
-owlCategory5.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevCatItem-1' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextCatItem-1' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
 
 /* ======================================== End Category Product ========================================================*/
 
 /* ======================================== Start Feature Product ========================================================*/
 
-var owlPromotion = $("#promotion-slider-1");
+var owlPromotion = $(".promotion-slider");
 owlPromotion.owlCarousel({
     items: 4 ,
     slideSpeed : 5000,
@@ -160,104 +45,11 @@ owlPromotion.owlCarousel({
     pagination : false,
 });
 
-var owlPromotion2 = $("#promotion-slider-2");
-owlPromotion2.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevPromotion-2' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextPromotion-2' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-// Custom Navigation Events
-$("#nextPromotion-2").click(function(){
-    owlPromotion2.trigger('owl.next');
-});
-$("#prevPromotion-2").click(function(){
-    owlPromotion2.trigger('owl.prev');
-});
-
-var owlPromotion3 = $("#promotion-slider-3");
-owlPromotion3.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevPromotion-3' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextPromotion-3' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-
-var owlPromotion4 = $("#promotion-slider-4");
-owlPromotion4.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevPromotion-4' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextPromotion-4' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-var owlPromotion5 = $("#promotion-slider-5");
-owlPromotion4.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevPromotion-5' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextPromotion-5' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
 /* ======================================== End Feature Product ========================================================*/
 
 /* ======================================== Start Feature Product ========================================================*/
 
-var owlTag = $("#tag-slider-1");
+var owlTag = $("#tag-slider");
 owlTag.owlCarousel({
     items: 4 ,
     slideSpeed : 5000,
@@ -278,114 +70,12 @@ owlTag.owlCarousel({
     pagination : false,
 });
 
-// Custom Navigation Events
-$("#nextTag-1").click(function(){
-    owlTag.trigger('owl.next');
-});
-$("#prevTag-1").click(function(){
-    owlTag.trigger('owl.prev');
-});
-
-
-var owlTag2 = $("#tag-slider-2");
-owlTag2.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevTag-2' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextTag-2' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-// Custom Navigation Events
-$("#nextTag-2").click(function(){
-    owlTag2.trigger('owl.next');
-});
-$("#prevTag-2").click(function(){
-    owlTag2.trigger('owl.prev');
-});
-
-var owlTag3 = $("#tag-slider-3");
-owlTag3.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevTag-3' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextTag-3' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-var owlTag4 = $("#tag-slider-4");
-owlTag4.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevTag-4' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextTag-4' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-
-var owlTag5 = $("#tag-slider-5");
-owlTag4.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevTag-5' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextTag-5' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
 
 /* ======================================== End Feature Product ========================================================*/
 
 /* ======================================== Start Discount Product ========================================================*/
 
-var owlDiscount = $("#discount-slider-1");
+var owlDiscount = $("#discount-slider");
 owlDiscount.owlCarousel({
     items: 4 ,
     slideSpeed : 5000,
@@ -406,99 +96,11 @@ owlDiscount.owlCarousel({
     pagination : false,
 });
 
-
-var owlDiscount2 = $("#discount-slider-2");
-owlDiscount2.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevDiscount-2' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextDiscount-2' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-var owlDiscount3 = $("#discount-slider-3");
-owlDiscount3.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevDiscount-3' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextDiscount-3' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-var owlDiscount4 = $("#discount-slider-4");
-owlDiscount4.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    itemsTabletSmall: false,
-    navigationText: [
-        "<a id='prevDiscount-4' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextDiscount-4' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-
-var owlDiscount5 = $("#discount-slider-5");
-owlDiscount4.owlCarousel({
-    items: 4 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevDiscount-5' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextDiscount-5' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-
-
 /* ======================================== End Feature Product ========================================================*/
 
 /* ======================================== Start Feature Product ========================================================*/
 
-var owlBrand = $("#brand-slider-1");
+var owlBrand = $("#brand-slider");
 owlBrand.owlCarousel({
     items: 6 ,
     slideSpeed : 5000,
@@ -519,57 +121,13 @@ owlBrand.owlCarousel({
     pagination : false,
 });
 
-
-var owlBrand2 = $("#brand-slider-2");
-owlBrand2.owlCarousel({
-    items: 6 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevBrand-2' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextBrand-2' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-
-var owlBrand3 = $("#brand-slider-3");
-owlBrand3.owlCarousel({
-    items: 6 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevBrand-3' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextBrand-3' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
 /* ======================================== End Feature Product ========================================================*/
 
 
 /* ======================================== Start Feature Category ========================================================*/
 
-var owlCategory = $("#feature-category-1");
-owlCategory.owlCarousel({
+var owlFeatureCategory = $("#feature-category");
+owlFeatureCategory.owlCarousel({
     items: 6 ,
     slideSpeed : 5000,
     itemsCustom : false,
@@ -584,49 +142,6 @@ owlCategory.owlCarousel({
     navigationText: [
         "<a id='prevCategory-1' class='glyphicon glyphicon-chevron-left'></a>",
         "<a id='nextCategory-1' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-
-var owlCategory2 = $("#brand-slider-2");
-owlCategory2.owlCarousel({
-    items: 6 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevCategory-2' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextCategory-2' class='glyphicon glyphicon-chevron-right'></a>"
-    ],
-    rewindNav : false,
-    pagination : false,
-});
-
-var owlCategory3 = $("#brand-slider-3");
-owlCategory3.owlCarousel({
-    items: 6 ,
-    slideSpeed : 5000,
-    itemsCustom : false,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [980,3],
-    itemsTablet: [768,2],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    itemsScaleUp : false,
-    autoPlay:false,
-    navigation : true,
-    navigationText: [
-        "<a id='prevCategory-3' class='glyphicon glyphicon-chevron-left'></a>",
-        "<a id='nextCategory-3' class='glyphicon glyphicon-chevron-right'></a>"
     ],
     rewindNav : false,
     pagination : false,
