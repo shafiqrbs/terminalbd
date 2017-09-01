a55d295fba6d77eb7eeff59b92e4d2ce8368466a
shafiq <shafiqabs@gmail.com>
2017-09-01 17:49:14 +0600

    Corporate Mobile Template Update.

--- src/Frontend/FrontentBundle/Controller/WebServiceProductController.php
+++ src/Frontend/FrontentBundle/Controller/WebServiceProductController.php
@@ -71,7 +71,7 @@ class WebServiceProductController extends Controller
             if( $detect->isMobile() || $detect->isTablet() ) {
                 $theme = 'Template/Mobile/'.$themeName;
             }else{
-                $theme = 'Template/Desktop/'.$themeName;
+                $theme = 'Template/Mobile/'.$themeName;
             }
 
 
