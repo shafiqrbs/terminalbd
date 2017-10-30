8346b9e4c8586f7827b271818564b984cd36a804
shafiq <shafiqabs@gmail.com>
2017-10-30 13:53:20 +0600

    Mobile Theme Update

--- src/Frontend/FrontentBundle/Resources/public/template/mobile/js/common.js
+++ src/Frontend/FrontentBundle/Resources/public/template/mobile/js/common.js
@@ -1,4 +1,39 @@
-    $(document).on( "click", ".emailSender", function(e){
+$(".carousel").carousel({
+    interval: 10000,
+    wrap:true
+});
+
+$(".carousel").on("slid", function() {
+    var to_slide;
+    to_slide = $(".carousel-item.active").attr("data-slide-no");
+    $(".myCarousel-target.active").removeClass("active");
+    $(".carousel-indicators [data-slide-to=" + to_slide + "]").addClass("active");
+});
+
+$(".myCarousel-target").on("click", function() {
+    $(this).preventDefault();
+    $(".carousel").carousel(parseInt($(this).attr("data-slide-to")));
+    $(".myCarousel-target.active").removeClass("active");
+    $(this).addClass("active");
+});
+
+
+$(".carousel").on("slid", function() {
+    var to_slide;
+    to_slide = $(".carousel-item.active").attr("data-slide-no");
+    $(".myCarousel-target.active").removeClass("active");
+    $(".carousel-indicators [data-slide-to=" + to_slide + "]").addClass("active");
+});
+
+$(".myCarousel-target").on("click", function() {
+    $(this).preventDefault();
+    $(".carousel").carousel(parseInt($(this).attr("data-slide-to")));
+    $(".myCarousel-target.active").removeClass("active");
+    $(this).addClass("active");
+});
+
+
+$(document).on( "click", ".emailSender", function(e){
 
         $.ajax({
             url         : $('#newsLetter').attr( 'action' ),
