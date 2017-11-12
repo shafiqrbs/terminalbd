739ec8c43d9a2cb0ff023b674d04b558f8b5e092
Roni Saha <roni.cse@gmail.com>
2017-11-12 14:40:32 +0600

    Update libraries to exact version

--- web/index.php
+++ web/index.php
@@ -1,9 +1,21 @@
 <?php
 ini_set('date.timezone', 'Asia/Dhaka');
+
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__.'/../app/autoload.php';
+
+EzMaintenance\Worker::watch('file', array(
+    'path' => 'maintenance.enable',
+    'template' => 'game',
+    'msg' => 'Site is currently undergoing maintenance!'
+));
+
 defined('APPLICATION_ENV')
 || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'prod'));
 
 define('WEB_PATH', __DIR__);
+
+$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 
 if (APPLICATION_ENV == 'dev') {
     require 'app_dev.php';
