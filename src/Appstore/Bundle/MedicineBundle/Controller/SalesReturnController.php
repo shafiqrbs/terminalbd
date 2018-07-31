f45645e12049dbdff37bdf0cac5c2d8d9f459146
shafiq <shafiqabs@gmail.com>
2018-07-31 18:11:54 +0600

    Medicine Sales Return update.

--- src/Appstore/Bundle/MedicineBundle/Controller/SalesReturnController.php
+++ src/Appstore/Bundle/MedicineBundle/Controller/SalesReturnController.php
@@ -50,20 +50,23 @@ class SalesReturnController extends Controller
         $config = $this->getUser()->getGlobalOption()->getMedicineConfig();
         $data = $request->request->all();
         foreach ($data['quantity'] as $key => $qnt) {
-            $entity = new MedicineSalesReturn();
-            $salesItem = $this->getDoctrine()->getRepository('MedicineBundle:MedicineSalesItem')->find($data['salesItem'][$key]);
-            $entity->setMedicineConfig($config);
-            $entity->setQuantity($qnt);
-            $entity->setMedicineStock($salesItem->getMedicineStock());
-            $entity->setMedicinePurchaseItem($salesItem->getMedicinePurchaseItem());
-            $entity->setMedicineSalesItem($salesItem);
-            $price = empty($data[$key])? $salesItem->getSalesPrice() : $data['salesPrice'][$key];
-            $entity->setSalesPrice($price);
-            $entity->setSubTotal($entity->getSalesPrice() * $entity->getQuantity());
-            $em->persist($entity);
-            $em->flush();
-            $this->getDoctrine()->getRepository('MedicineBundle:MedicinePurchaseItem')->updateRemovePurchaseItemQuantity($salesItem->getMedicinePurchaseItem(), 'damage');
-            $this->getDoctrine()->getRepository('MedicineBundle:MedicineStock')->updateRemovePurchaseQuantity($salesItem->getMedicineStock(), 'damage');
+        	if($qnt > 0 ){
+		        $entity = new MedicineSalesReturn();
+		        $salesItem = $this->getDoctrine()->getRepository('MedicineBundle:MedicineSalesItem')->find($data['salesItem'][$key]);
+		        $entity->setMedicineConfig($config);
+		        $entity->setQuantity($qnt);
+		        $entity->setMedicineStock($salesItem->getMedicineStock());
+		        $entity->setMedicinePurchaseItem($salesItem->getMedicinePurchaseItem());
+		        $entity->setMedicineSalesItem($salesItem);
+		        $price = empty($data[$key])? $salesItem->getSalesPrice() : $data['salesPrice'][$key];
+		        $entity->setSalesPrice($price);
+		        $entity->setSubTotal($entity->getSalesPrice() * $entity->getQuantity());
+		        $em->persist($entity);
+		        $em->flush();
+		        $this->getDoctrine()->getRepository('MedicineBundle:MedicinePurchaseItem')->updateRemovePurchaseItemQuantity($salesItem->getMedicinePurchaseItem(), 'sales-return');
+		        $this->getDoctrine()->getRepository('MedicineBundle:MedicineStock')->updateRemovePurchaseQuantity($salesItem->getMedicineStock(), 'sales-return');
+
+	        }
         }
         $this->get('session')->getFlashBag()->add(
             'success',"Data has been inserted successfully"
@@ -96,7 +99,7 @@ class SalesReturnController extends Controller
     	$em = $this->getDoctrine()->getManager();
         if (!empty($entity) and $entity->getProcess() !='approved') {
             $em = $this->getDoctrine()->getManager();
-            $entity->setProcess('approved');
+	        $entity->setProcess('approved');
             $journal = $em->getRepository('AccountingBundle:AccountJournal')->insertMedicineAccountSalesReturn($entity);
 	        $entity->setJournal($journal);
             $em->flush();
