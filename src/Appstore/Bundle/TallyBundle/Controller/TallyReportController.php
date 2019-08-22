5f21b068a393bee0bb1a5b1e3f89b7a5cb844ae0
rakeybulhasan <rakeybulhasan@emicrograph.com>
2019-08-22 19:32:04 +0600

    tally report added

---
+++ src/Appstore/Bundle/TallyBundle/Controller/TallyReportController.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hasan
+ * Date: 8/20/19
+ * Time: 11:07 AM
+ */
+
+namespace Appstore\Bundle\TallyBundle\Controller;
+
+
+use Knp\Snappy\Pdf;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Component\HttpFoundation\Response;
+use TCPDF;
+
+class TallyReportController extends Controller
+{
+
+    public function mushukFourThreeAction(){
+
+        $html = $this->renderView('TallyBundle:Report:mushok_4_3.html.twig');
+        $wkhtmltopdfPath = 'xvfb-run --server-args="-screen 0, 1280x1024x24" /usr/local/bin/wkhtmltopdf --header-html';
+        $snappy = new Pdf($wkhtmltopdfPath);
+        $snappy->setOption('page-size', 'A4');
+        $snappy->setOption('orientation', 'Landscape');
+
+        return new Response(
+            $snappy->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+//                'Content-Disposition'   => 'attachment; filename="file.pdf"'
+
+            )
+        );
+
+
+    }
+
+    public function mushukSixOneAction(){
+
+        $html = $this->renderView('TallyBundle:Report:mushok_6_1.html.twig');
+
+        $wkhtmltopdfPath = 'xvfb-run --server-args="-screen 0, 1280x1024x24" /usr/bin/wkhtmltopdf --use-xserver';
+        $snappy          = new Pdf($wkhtmltopdfPath);
+        $snappy->setOption('orientation', 'Landscape');
+        $snappy->setOption('page-size', 'A4');
+        $pdf             = $snappy->getOutputFromHtml($html);
+        header('Content-Type: application/pdf');
+//        header('Content-Disposition: attachment; filename="monthlyIncomePdf.pdf"');
+        echo $pdf;
+        return new Response('');
+    }
+
+    public function mushukSixTwoAction(){
+
+        $html = $this->renderView('TallyBundle:Report:mushok_6_2.html.twig');
+
+        $wkhtmltopdfPath = 'xvfb-run --server-args="-screen 0, 1280x1024x24" /usr/bin/wkhtmltopdf --use-xserver';
+        $snappy          = new Pdf($wkhtmltopdfPath);
+        $snappy->setOption('orientation', 'Landscape');
+        $snappy->setOption('page-size', 'A4');
+        $pdf             = $snappy->getOutputFromHtml($html);
+        header('Content-Type: application/pdf');
+//        header('Content-Disposition: attachment; filename="monthlyIncomePdf.pdf"');
+        echo $pdf;
+        return new Response('');
+    }
+
+    public function mushukSixTwoOneAction(){
+
+        $html = $this->renderView('TallyBundle:Report:mushok_6_2_1.html.twig');
+        $wkhtmltopdfPath = 'xvfb-run --server-args="-screen 0, 1280x1024x24" /usr/local/bin/wkhtmltopdf --header-html';
+        $snappy = new Pdf($wkhtmltopdfPath);
+        $snappy->setOption('page-size', 'A4');
+        $snappy->setOption('orientation', 'Landscape');
+
+        return new Response(
+            $snappy->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+//                'Content-Disposition'   => 'attachment; filename="file.pdf"'
+
+            )
+        );
+
+
+    }
+
+    public function mushukSixThreeAction(){
+
+        $html = $this->renderView('TallyBundle:Report:mushok_6_3.html.twig');
+        $wkhtmltopdfPath = 'xvfb-run --server-args="-screen 0, 1280x1024x24" /usr/local/bin/wkhtmltopdf --header-html';
+        $snappy = new Pdf($wkhtmltopdfPath);
+        $snappy->setOption('page-size', 'A4');
+        $snappy->setOption('orientation', 'Landscape');
+
+        return new Response(
+            $snappy->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+//                'Content-Disposition'   => 'attachment; filename="file.pdf"'
+
+            )
+        );
+
+
+    }
+
+
+}
