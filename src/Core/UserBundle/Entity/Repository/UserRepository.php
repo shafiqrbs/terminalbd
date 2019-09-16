0549291ddab5148cd802126ee1898047382d0e83
shafiq <shafiqabs@gmail.com>
2019-09-16 11:51:24 +0600

    Customer Information update.

--- src/Core/UserBundle/Entity/Repository/UserRepository.php
+++ src/Core/UserBundle/Entity/Repository/UserRepository.php
@@ -318,7 +318,7 @@ class UserRepository extends EntityRepository
             'ROLE_CUSTOMER_REHAB'               => 'Rehab Patient',
             'ROLE_CRM_MANAGER'                  => 'Manage Customer ',
             'ROLE_MEMBER_ASSOCIATION'           => 'Association',
-            'ROLE_MEMBER_ASSOCIATION_MODERATOR'   => 'Association Moderator',
+            'ROLE_MEMBER_ASSOCIATION_MODERATOR' => 'Association Moderator',
             'ROLE_MEMBER_ASSOCIATION_ADMIN'     => 'Association Admin',
         );
 
