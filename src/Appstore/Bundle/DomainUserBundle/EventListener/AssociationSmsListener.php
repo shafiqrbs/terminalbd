e08c3842b91b078483e71e090f67c52295939a9e
shafiq <shafiqabs@gmail.com>
2019-11-20 21:32:38 +0600

    Various issues are done.

--- src/Appstore/Bundle/DomainUserBundle/EventListener/AssociationSmsListener.php
+++ src/Appstore/Bundle/DomainUserBundle/EventListener/AssociationSmsListener.php
@@ -37,7 +37,7 @@ class AssociationSmsListener extends BaseSmsAwareListener
         $post = $event->getCustomer();
         $msg = $event->getMemberMsg();
         if($post->getCountry()->getName() == "Bangladesh"){
-            $mobile = "+88".$post->getMobile();
+            $mobile = "88".$post->getMobile();
             $status = $this->gateway->send($msg, $mobile);
             $this->em->getRepository('SettingToolBundle:SmsSender')->insertCustomerSenderSms($post,$msg, $status);
         }elseif($post->getCountry() and $post->getCountry()->getName() != "Bangladesh"){
