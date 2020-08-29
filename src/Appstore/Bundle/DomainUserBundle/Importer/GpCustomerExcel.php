6e4b1adcc97706298910df00545f9929869900f5
shafiq <shafiqabs@gmail.com>
2020-08-29 09:56:32 +0600

    Business Invoice :
    Custome invoice design done for nahar

--- src/Appstore/Bundle/DomainUserBundle/Importer/GpCustomerExcel.php
+++ src/Appstore/Bundle/DomainUserBundle/Importer/GpCustomerExcel.php
@@ -42,9 +42,9 @@ class GpCustomerExcel
     }
 
     function clean($string) {
-        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
-
-        return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+        $string = str_replace(' ', '', $string); // Replaces all spaces with hyphens.
+        $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+        return preg_replace('/-+/', '', $string); // Replaces multiple hyphens with single one.
     }
 
     public function setGlobalOption($option)
