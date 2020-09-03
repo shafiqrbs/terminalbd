9c709895d8e28fb15c751acdbfdb006b26d10fa5
shafiq <shafiqabs@gmail.com>
2020-09-03 08:23:49 +0600

    All Income Statement Okay. Missing Mobile bank account for Central Kitchen

--- src/Appstore/Bundle/AccountingBundle/Repository/AccountSalesRepository.php
+++ src/Appstore/Bundle/AccountingBundle/Repository/AccountSalesRepository.php
@@ -423,7 +423,6 @@ class AccountSalesRepository extends EntityRepository
         $qb = $this->createQueryBuilder('e');
         $qb->select('COALESCE(SUM(e.totalAmount),0) AS salesAmount','COALESCE(SUM(e.purchasePrice),0) AS purchaseAmount','COALESCE(SUM(e.vat),0) AS vatAmount');
         $qb->where("e.globalOption = :globalOption")->setParameter('globalOption', $globalOption);
-        $qb->andWhere('e.processHead IN(:process)');
         $qb->andWhere('e.processType = :type')->setParameter('type', 'Sales');
         $this->handleSearchBetween($qb,$data);
         $result  = $qb->getQuery()->getOneOrNullResult();
