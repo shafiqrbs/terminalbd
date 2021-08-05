d86e36ad54ab01a19abf6443d58093af4f41cfc1
shafiq <shafiqabs@gmail.com>
2021-08-05 16:39:14 +0600

    Print update.

--- src/Appstore/Bundle/MedicineBundle/Form/SalesTemporaryItemType.php
+++ src/Appstore/Bundle/MedicineBundle/Form/SalesTemporaryItemType.php
@@ -34,6 +34,7 @@ class SalesTemporaryItemType extends AbstractType
                 'expanded'      =>false,
                 'empty_value' => '-Disc(%)-',
                 'mapped' => false,
+                'required'    => false,
                 'multiple'      =>false,
                 'choices' => array(1,2,3,4,5,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,),
             ))
