b105f637efa672a89fcf731c87d22703b031ceac
shafiq <shafiqabs@gmail.com>
2021-10-02 16:05:45 +0600

    Admission people edit issue.

--- src/Appstore/Bundle/HospitalBundle/Form/NewPatientAdmissionType.php
+++ src/Appstore/Bundle/HospitalBundle/Form/NewPatientAdmissionType.php
@@ -87,7 +87,7 @@ class NewPatientAdmissionType extends AbstractType
                 'query_builder' => function(EntityRepository $er){
                     return $er->createQueryBuilder('b')
                         ->where("b.status = 1")
-                        ->andWhere("b.service = 6")
+                        ->andWhere("b.service = 5")
                         ->andWhere("b.hospitalConfig =".$this->globalOption->getHospitalConfig()->getId())
                         ->orderBy("b.name", "ASC");
                 }
