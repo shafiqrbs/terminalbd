850fbb6ccaffbaee704b58fe748601db3e803703
Shafiq Isalm <shafiqabs@gmail.com>
2022-07-30 13:38:23 +0600

    Script update.

--- app/Resources/assets/scripts/script.js
+++ app/Resources/assets/scripts/script.js
@@ -3,20 +3,45 @@
  **/
 var AppScript = function () {
 
-    $('.dataTable').DataTable( {
-        iDisplayLength: 25,
-        scrollY:        '50vh',
-        scrollX: false,
-        scrollCollapse: true,
-        paging:         false,
-        bInfo : false,
-        orderable: true,
-        bSort: true,
-        aoColumnDefs: [
-            {
-                bSortable: false,
-                aTargets: [ -1 ]
+    var tableTable = function () {
+        $('.dataTable').DataTable({
+            iDisplayLength: 25,
+            scrollY: '50vh',
+            scrollX: false,
+            scrollCollapse: true,
+            paging: false,
+            bInfo: false,
+            orderable: true,
+            bSort: true,
+            aoColumnDefs: [
+                {
+                    bSortable: false,
+                    aTargets: [-1]
+                }
+            ]
+        });
+    }
+    var tableInlineNext = function () {
+        $('body').on('keydown', '.td-inline-input', function(event) {
+            var key = event.which || event.charCode || event.keyCode
+            if (key === 13) {
+                var self = $(this), form = self.parents('form:eq(0)'), focusable, next;
+                focusable = form.find('.td-inline-input').filter(':visible');
+                next = focusable.eq(focusable.index(this)+1);
+                if (next.length) {
+                    next.focus();
+                }
+                return false;
             }
-        ]
-    });
-};
+        });
+    }
+    return {
+        //main function to initiate the module
+        init: function () {
+            tableTable();
+            tableInlineNext();
+        }
+
+    };
+
+}();
