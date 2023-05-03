47db4af8a6ccb840ecd64057600a30ba19d0e6e4
Shafiq Isalm <shafiqabs@gmail.com>
2023-05-04 03:21:26 +0600

    Domain controller update.

--- src/Setting/Bundle/ToolBundle/Controller/DomainController.php
+++ src/Setting/Bundle/ToolBundle/Controller/DomainController.php
@@ -92,6 +92,9 @@ class DomainController extends Controller
 
     public function databaseDumpAction(){
 
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
 
         $DBUSER="root";
         $DBPASSWD="+Hq3#Q)ixsVN.jV}";
